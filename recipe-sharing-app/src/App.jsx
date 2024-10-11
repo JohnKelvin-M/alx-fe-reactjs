8558e206e3eacb92e3a4fc545e0c459703d0692d
JohnKelvin-M <kelvinjohn254@gmail.com>
2024-10-11 18:54:17 +0300

    Added recipe management features10

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,14 +1,26 @@
+// src/App.jsx
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import SearchBar from './components/SearchBar';
+import RecipeDetails from './components/RecipeDetails';
+import AddRecipeForm from './components/AddRecipeForm';
 
 const App = () => {
   return (
-    <div>
-      <h1>Recipe Sharing App</h1>
-      <SearchBar />
-      <RecipeList />
-    </div>
+    <BrowserRouter>
+      <div>
+        <h1>Recipe Sharing App</h1>
+        {/* The SearchBar will be visible on every route */}
+        <SearchBar />
+        
+        <Routes>
+          <Route path="/" element={<RecipeList />} />
+          <Route path="/recipe/:id" element={<RecipeDetails />} />
+          <Route path="/add" element={<AddRecipeForm />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 };
 
