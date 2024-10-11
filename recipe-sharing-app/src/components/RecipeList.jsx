e6107b85a08d408916a66988fc85edb2e3010c7b
JohnKelvin-M <kelvinjohn254@gmail.com>
2024-10-11 18:53:11 +0300

    Added recipe management features9

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,17 +1,24 @@
+// src/components/RecipeList.jsx
 import React from 'react';
-import { useRecipeStore } from './recipeStore';
+import { Link } from 'react-router-dom';
+import { useRecipeStore } from '../recipeStore';
 
 const RecipeList = () => {
-  const filteredRecipes = useRecipeStore((state) => state.filteredRecipes);
+  const recipes = useRecipeStore((state) => state.filteredRecipes);
 
   return (
     <div>
-      {filteredRecipes.map((recipe) => (
-        <div key={recipe.id}>
-          <h3>{recipe.title}</h3>
-          <p>{recipe.description}</p>
-        </div>
-      ))}
+      <h2>Recipe List</h2>
+      <ul>
+        {recipes.map((recipe) => (
+          <li key={recipe.id}>
+            {/* Link to individual recipe detail page */}
+            <Link to={`/recipe/${recipe.id}`}>
+              {recipe.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
